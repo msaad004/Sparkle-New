be9f8c9c9ba45bf066796a9c493777a11004de13
Md Sa'ad Fareed <111727930+msaad004@users.noreply.github.com>
2024-01-17 14:51:52 +0500

    commit4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     <title>Sparkle</title>
   </head>
   <body>
