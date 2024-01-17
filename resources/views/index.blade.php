324151b4093fdaa5b12e00dc082031f895b542a7
Md Sa'ad Fareed <111727930+msaad004@users.noreply.github.com>
2024-01-17 14:56:28 +0500

    saad

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
   </head>
   <body>
     @include('navbar')
-    
+      <h1>Sparkle Welcomes You...</h1>
     <script src="{{ asset('jquery/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('popper/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
