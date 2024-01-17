d9644c073b2a60518c0e6bd1596d6673c1e3d34f
Md Sa'ad Fareed <111727930+msaad004@users.noreply.github.com>
2024-01-17 15:22:00 +0500

    navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -10,7 +10,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item me-5">
-              <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
+              <a class="nav-link" aria-current="page" href="{{ url('/') }}">Home</a>
             </li>
             <li class="nav-item me-5">
               <a class="nav-link" href="{{ url('add-spark') }}">Post Spark</a>
